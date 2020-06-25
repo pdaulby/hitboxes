3d077a1e6ff83d699b54dc7b410075b5bb43c695
unknown <p.daulby@hotmail.com>
2020-06-25 11:32:34 +0100

    add basic hitbox hurtbox, currently only allows editing of hitbox

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Box from 'src/types/box';
+import Box, { BoxDefinition, Boxes } from 'src/types/box';
 
 @Component({
   selector: 'app-frame',
@@ -10,36 +10,43 @@ export class FrameComponent implements OnInit {
 
   constructor() { }
   
-  imageUrl: string = "https://i.imgur.com/im8dAFe.jpeg";
-  boxes: Box[] = [
-    {square: {bottom: 10, left: 10, top: 80, right: 80 }},
-    {square: {bottom: 20, left: 20, top: 90, right: 90 }}
+  boxTypes: BoxDefinition[] = [
+    {name: "hurtbox", color: "green", numbers: []},
+    {name: "hitbox", color: "red", numbers: ["damage"]},
   ]
+
+  imageUrl: string = "https://i.imgur.com/im8dAFe.jpeg";
+  boxes: Boxes = new Boxes(this.boxTypes);
 
   getRedraw() {
     var b = this.boxes;
     return ()=>this.redraw(b);
   }
 
-  redraw(boxes: Box[]) {
+  redraw(boxes: Boxes) {
     var canvas = document.querySelector("canvas");
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.lineWidth = 2;
-    boxes.forEach(box => {
-      context.beginPath();
-      context.rect(box.square.bottom, box.square.left, box.square.top, box.square.right);
-      context.strokeStyle = "red";
-      context.stroke();
-    });
+    this.boxTypes.forEach(definition=> {
+      context.strokeStyle = definition.color;
+      boxes.boxes.get(definition.name).forEach(box=>{
+        context.beginPath();
+        context.rect(box.square.bottom, box.square.left, box.square.top, box.square.right);
+        context.stroke();
+      })
+    })
   }
 
   ngOnInit(): void {
+    this.boxes.add("hurtbox", {square: {bottom: 110, left: 110, top: 180, right: 180 }});
+    this.boxes.add("hurtbox", {square: {bottom: 120, left: 120, top: 280, right: 280 }});
+    this.boxes.add("hitbox", {square: {bottom: 150, left: 120, top: 280, right: 250 }});
     this.redraw(this.boxes);
   }
 
   addBox() {
-    this.boxes.push({square: {bottom: 0, left: 0, top: 0, right: 0 }})
+    this.boxes.add("hitbox", {square: {bottom: 0, left: 0, top: 0, right: 0 }})
   }
 
 
--- src/types/Box.ts
+++ src/types/Box.ts
@@ -6,3 +6,24 @@ export default interface Box {
     right: number
   }
 }
+
+export interface BoxDefinition {
+  name: string;
+  color: string;
+  numbers: string[];
+}
+
+export class Boxes {
+  boxes: Map<string, Box[]>;
+
+  constructor (definitions: BoxDefinition[]){
+    this.boxes = new Map();
+    definitions.map(d=>d.name).forEach(name=>{
+      this.boxes.set(name, [])
+    });
+  }
+
+  add(name: string, box: Box){
+    this.boxes.get(name).push(box);
+  }
+}
